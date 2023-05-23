8870fb402c6db02c189b91aaa32e4a372b8d8cee
hungnc89 <72601287+hungnc89@users.noreply.github.com>
2023-05-23 18:57:21 +0700

    Update install_mirotalk.sh

--- install_mirotalk.sh
+++ install_mirotalk.sh
@@ -32,7 +32,7 @@ server {
     server_name $domain_name;
 
     location / {
-        proxy_pass http://localhost:3010;
+        proxy_pass http://localhost:3000;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection 'upgrade';
